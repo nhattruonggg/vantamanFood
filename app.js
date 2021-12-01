2ff085089838ccb7a41f73dc9610cd7bfd3f551f
Truong <nguyennhattruong1717@gmail.com'you@example.com>
2021-12-01 15:57:43 +0700

    upload

---
+++ app.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){
+    const slider = document.querySelector('.slider');
+    const sliderMain = document.querySelector('.slider-main');
+    const sliderItems = document.querySelectorAll('.slider-item');
+    const nextBtn =  document.querySelector('.slider-next');
+    const prevBtn =  document.querySelector('.slider-prev');
+    const dotsItem = document.querySelectorAll('.slider-dot-item');
+    const sliderLength = sliderItems.length;
+    sliderMain.style.width = `${sliderLength * sliderItemsWidth} px`
+    const sliderItemsWidth = sliderItems[0].offsetWidth;
+    
+
+})
